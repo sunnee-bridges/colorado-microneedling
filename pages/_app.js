3c778b4d75ca70e40ffe2cf099583ad72948a7c4
Nick Matthews <nmatthews@t3rdeyetech.com>
2025-09-02 22:43:52 -0600

    add router for metrics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
-// pages/_app.js
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!GA_ID) return;
+    const handleRouteChange = (url) => window.gtag?.("config", GA_ID, { page_path: url });
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
+  return (
+    <>
+      {GA_ID && (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+          <Script id="ga-init" strategy="afterInteractive">{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}</Script>
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
